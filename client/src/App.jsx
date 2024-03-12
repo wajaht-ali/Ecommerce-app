15deecb3bf0cd7b2d727a2da81df8797cd9c681b
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2024-03-12 12:58:48 +0500

    Routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable no-unused-vars */
-import Layout from './components/layout/Layout';
 import './styles/App.css';
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Policy from "./pages/Policy.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <Layout>
-      <h1>Ecommerce App</h1>
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/policy" element={<Policy />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
   )
 }
 
