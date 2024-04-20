c7193cf405ab0eba5d9c0db0baf09c781f695337
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2024-04-20 17:18:56 +0500

    Home page editing and fixing

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from '../components/layout/Layout.jsx';
 import { useAuth } from '../context/Auth.jsx';
 import SearchInput from './SearchInput.jsx';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   const [auth, setAuth] = useAuth();
@@ -23,15 +24,15 @@ const Home = () => {
                 Sed ut perspiciatis unde omnis iste natus voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
               </p>
               <div className="items-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
-                <a href="javascript:void(0)" className="block py-2 px-4 text-center text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
-                  Let&apos;s get started
-                </a>
-                <a href="javascript:void(0)" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
-                  Get access
+                <Link to="/products" className="block py-2 px-4 text-center text-white font-medium bg-indigo-600 duration-150 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg shadow-lg hover:shadow-none">
+                  View Category
+                </Link>
+                <Link to="/products" className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex">
+                  Products
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                     <path fillRule="evenodd" d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z" clipRule="evenodd" />
                   </svg>
-                </a>
+                </Link>
               </div>
             </div>
             <div className="flex-none mt-14 md:mt-0 md:max-w-xl">
